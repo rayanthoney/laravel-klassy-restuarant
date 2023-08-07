71fd3be27f25aae6b86d2ad15c428d6409058f02
rayanthoney <rmaxey911@gmail.com>
2023-08-07 10:00:29 -0600

    added reservation Controlor, Table, and  migration

---
+++ resources/views/admin/updateview.blade.php
@@ -0,0 +1,51 @@
+<x-app-layout>
+
+</x-app-layout>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <base href="/public">
+    @include("admin.admincss")
+
+  </head>
+  <body> 
+
+    <div class="container-scroller">
+        @include("admin.navbar");
+
+        <div style="position:  relative; top: 60px; right: -150px">
+            <form action="{{ url('/update',$data->id) }}" method="post" enctype="multipart/form-data">
+
+                @csrf
+
+                <div>
+                    <label for="">Title</label>
+                    <input style="color: blue" type="text" name="title" id="" value="{{ $data->title }}" required>
+                </div>
+                <div>
+                    <label for="">Price</label>
+                    <input style="color: blue" type="number" name="price" id="" value="{{ $data->price }}" required>
+                </div>
+                <div>
+                    <label for="">Description</label>
+                    <input style="color: blue" type="text" name="description" id="" value="{{ $data->description }}" required>
+                </div>
+                <div>
+                    <label for="">oldimage</label>
+                    <img height="200" width="200" src="/foodimage/{{ $data->image }}" alt="">
+                </div>
+                <div>
+                    <label for=""Image</label>
+                    <input type="file" name="image" required'>
+                </div>
+                <div>
+                    <input style="color: black" type="submit" value="Save">
+                </div>
+            </form>
+        </div>
+
+    @include("admin.adminscript")
+  </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+Route::get("/",[HomeController::class,"index"]);
+
+// I am removing the route redirects. I am testing it auto populating:::
+Route::get("/redirects",[HomeController::class,"redirects"]);
+
+Route::get("/users",[AdminController::class,"user"]);
+
+Route::get("/deletemenu/{id}",[AdminController::class,"deletemenu"]);
+
+Route::get("/foodmenu",[AdminController::class,"foodmenu"]);
+
+Route::post("/uploadfood",[AdminController::class,"upload"]);
+
+Route::get("/deleteuser/{id}",[AdminController::class,"deleteuser"]);
+
+Route::get("/updateview/{id}",[AdminController::class,"updateview"]);
+
+Route::post("/update/{id}",[AdminController::class,"update"]);
+
+Route::post("/reservation",[AdminController::class,"reservation"]);
+
+
+
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
