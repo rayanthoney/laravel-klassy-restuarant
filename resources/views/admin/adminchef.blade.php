5d614d732d72f40b6638d1437ae5e2b4ddb42b9e
rayanthoney <rmaxey911@gmail.com>
2023-08-11 20:03:13 -0600

    created the adminchef

--- resources/views/admin/adminchef.blade.php
+++ resources/views/admin/adminchef.blade.php
@@ -12,10 +12,28 @@
   <body> 
 
     <div class="container-scroller">
-      @include("admin.navbar");
+      @include("admin.navbar")
 
-      <h1>Admin Chef</h1>
-    </div>
+      <form action="{{ url('/uploadchef') }}" method="Post" enctype="multipart/form-data">
+
+        @csrf
+        
+        <div>
+          <label for="">Name</label>
+          <input style="color:blue;" type="text" name="name" required placeholder="Enter Name">
+        </div>
+        <div>
+          <label for="">Speciality</label>
+          <input style="color:blue;" type="text" name="speciality" placeholder="Enter Speciality" required>
+        </div>
+        <div>
+            <input type="file" name="image" required>
+        </div>
+        <div>
+          <input style="color:blue;" type="submit" value="Save" id="">
+        </div>
+      </form>
+    
 
     @include("admin.adminscript")
   </body>
