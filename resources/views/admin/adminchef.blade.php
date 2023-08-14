621be36860a1503892d7096037f00f53b3e5d863
rayanthoney <rmaxey911@gmail.com>
2023-08-14 11:43:25 -0600

    added new view

--- resources/views/admin/adminchef.blade.php
+++ resources/views/admin/adminchef.blade.php
@@ -40,6 +40,7 @@
           <th style="padding: 30px;">Speciality</th>
           <th style="padding: 30px;">Image</th>
           <th style="padding: 30px;">Action</th>
+          <th style="padding: 30px;">Action2</th>
         </tr>
 
         @foreach ($data as $data)
@@ -48,6 +49,7 @@
           <td>{{ $data->speciality }}</td>
           <td><img height="100px" width="100px" src="/chefimage/{{ $data->image }}" alt=""></td>
           <td><a href="{{ url('/updatechef',$data->id) }}">Update</a></td>
+          <td><a href="{{ url('/deletechef',$data->id) }}">Delete</a></td>
         </tr>          
         @endforeach
 
