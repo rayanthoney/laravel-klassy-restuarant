de77753e82d32eb282ec58f08c584d6fd8c8f6b8
rayanthoney <rmaxey911@gmail.com>
2023-08-15 07:16:24 -0600

    added routes

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,8 @@ Route::post("/updatefoodchef/{id}",[AdminController::class,"updatefoodchef"]);
 
 Route::get("/deletechef/{id}",[AdminController::class,"deletechef"]);
 
+Route::post("/addcart/{id}",[HomeController::class,"addcart"]);
+
 
 
 
