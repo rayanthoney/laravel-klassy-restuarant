74d2b1874e130ce905caf25dd911e47c4c462f61
rayanthoney <rmaxey911@gmail.com>
2023-08-15 21:25:31 -0600

    added data table for the showcart

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Food;
 use App\Models\Foodchef;
+use App\Models\Cart;
 
 class HomeController extends Controller
 {
@@ -14,7 +15,7 @@ class HomeController extends Controller
     {
         $data = food::all();
 
-        $data2=foodchef::all();
+        $data2 = foodchef::all();
 
         return view('home', compact("data", "data2"));
     }
@@ -22,12 +23,51 @@ class HomeController extends Controller
     public function redirects()
     {
         $data = food::all();
+        $data2 = foodchef::all();
         $usertype = Auth::user()->usertype;
 
         if ($usertype == '1') {
             return view('admin.adminhome');
         } else {
-            return view('home',compact("data"));
+
+            $user_id = Auth::id();
+
+            $count = cart::where('user_id', $user_id)->count();
+
+
+            return view('home', compact("data", "data2", "count"));
         }
+    }
+
+    public function addcart(Request $request, $id)
+    {
+        if (Auth::id()) {
+            $user_id = Auth::id();
+            $foodid = $id;
+
+            $quantity = $request->quantity;
+
+            $cart = new cart;
+
+            $cart->user_id = $user_id;
+            $cart->food_id = $foodid;
+            $cart->quantity = $quantity;
+
+            $cart->save();
+
+            return redirect()->back();
+        } else {
+            return redirect('/login');
+        }
+    }
+
+    public function showcart(Request $request, $id)
+    {
+
+        $count = cart::where('user_id', $id)->count();
+
+        $data = cart::where('user_id', $id)->join('food', 'carts.food_id', '=', 'food.id')->get();
+
+        return view('showcart', compact('count', 'data'));
     }
 }
